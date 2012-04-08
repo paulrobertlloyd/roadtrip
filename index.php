a33663d5b8fbe1fe03ce4cfc7ce157e707e00cd0
Paul Robert Lloyd <github@paulrobertlloyd.com>
2012-04-08 18:48:23 +0100

    Base file setup

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="author" content="Paul Robert Lloyd"/>
+    <meta name="description" content=""/>
+
+    <!--css-->
+    <link rel="stylesheet" href="/_css/core.css" type="text/css" media="all"/>
+
+    <!--icons-->
+    <link rel="shortcut icon" href="/_assets/favicon.ico" type="image/x-icon"/>
+    <link rel="apple-touch-icon-precomposed" href="/_assets/apple-touch-icon-precomposed.png" type="image/png"/>
+
+    <!--meta-->
+    <title>Road Trip: Austin, TX - San Francisco, CA</title>
+</head>
+
+<body>
+    <div role="banner" class="banner">
+        <h1><a href="/" rel="home">Road Trip: Austin, <abrr title="Texas">TX</abbr> - San Francisco, <abbr title="California">CA</abbr></a></h1>
+
+        <ul role="navigation" class="navigation">
+            <li><a class="nav-notes" href="#">Route</a></li>
+            <li><a class="nav-blog" href="#">Blog</a></li>
+            <li><a class="nav-gallery" href="#">Gallery</a></li>
+            <li><a class="nav-about" href="#">About</a></li>
+        </ul><!--/navigation-->
+    </div><!--/banner-->
+
+    <div role="document" class="document">
+
+        <figure>
+            <img src="http://dummyimage.com/800x536/ccc/eee.png" alt="Arriving in Las Vagas"/>
+        </figure>
+
+        <nav role="navigation" class="item">
+            <ul>
+                <li class="item-prev"><a href="#">&#8592; Prev</a></li>
+                <li class="item-next"><a href="#">Next &#8594;</a></li>
+            </ul>
+        </nav>
+
+        <div role="main" class="main">
+            <h1>Arriving in Las Vagas</h1>
+            <p class="mile">Mile 1456</p>
+
+            <h2>Second-level heading</h2>
+            <h3>Third-level heading</h3>
+            <p>The <strong>Las Vegas Strip</strong> is an approximately 4.2-mile (6.8 km) stretch of Las Vegas Boulevard in Clark County, Nevada; adjacent to, but outside the city limits of Las Vegas proper. The Strip lies within the unincorporated townships of Paradise and Winchester. Most of the Strip has been designated an All-American Road.</p>
+            <p>Many of the largest hotel, casino and resort properties in the world are located on the Las Vegas Strip. Nineteen of the world&#8217;s 25 largest hotels by room count are on the Strip, with a total of over 67,000 rooms. One of the 19, the LVH - Las Vegas Hotel &#38; Casino, (Formerly the Las Vegas Hilton), is an &#8216;off-Strip&#8217; property but is located less than 0.5 miles (0.8 km) east of the Strip.</p>
+            <p>One of the most visible aspects of Las Vegas&#8217; cityscape is its use of dramatic architecture. The modernisation of hotels, casinos, restaurants, and residential high-rises on the Strip has established the city as one of the most popular destinations for tourists.</p>
+            <p class="caption"><a href="#">Text from Wikipedia</a>. Some rights reserved.</p>
+
+            <header>
+                <h1>Day 1</h1>
+                <h2>Austin, <abbr title="Texas">TX</abbr> - Albuquerque, <abbr title="New Mexico">NM</abbr></h2>
+                <p>785 Miles</p>
+            </header>
+
+            <ol>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+                <li>
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Title of media item"/>
+                    <h3>Title of media item</h3>
+                    <p class="datetime">10:04PM</p>
+                </li>
+            </ol>
+
+            <p>
+                <img src="http://dummyimage.com/60x60/ccc/eee.png" alt="Paul Robert Lloyd"/>
+                Taken by Paul<br/>
+                <span class="datetime">13 March 2012 at 10:04PM</span>
+            </p>
+
+            <figure>
+                <div class="map">
+                    <img src="http://dummyimage.com/240x160/ccc/eee.png" alt="Location of Las Vegas Strip"/>
+                </div>
+                <figcaption>
+                    <p>Taken on the Las Vegas Strip, Las Vegas, Nevada.</p>
+                </figcaption>
+            </figure>
+
+            <nav role="navigation" class="page">
+                <ul>
+                    <li class="page-prev"><a href="#">&#8592; Prev</a></li>
+                    <li class="page-current"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">6</a></li>
+                    <li class="page-next"><a href="#">Next &#8594;</a></li>
+                </ul>
+            </nav>
+        </div><!--/main-->
+
+        <div role="complementary" class="complementary">
+            <h1>What is this site all about?</h1>
+            <p>On March 12 2012, three web developers left the throngs of SXSW in Austin, and headed west towards California. Covering 5 states over 4 nights and 3 days, they arrived in San Francisco on March 15. But really, this is just some placeholder content to demonstrate how one might go about designing a responsive website.</p>
+            <p><a href="#">More about this road trip</a></p>
+
+            <h2>The Travellers</h2>
+            <h3>Paul Lloyd</h3>
+            <p>
+                <img src="http://dummyimage.com/60x60/ccc/eee.png" alt=""/>
+                <a href="http://paulrobertlloyd.com/">paulrobertlloyd.com</a><br/>
+                <a href="http://twitter.com/paulrobertlloyd/">@paulrobertlloyd</a>
+            </p>
+
+            <h3>Andy Hume</h3>
+            <p>
+                <img src="http://dummyimage.com/60x60/ccc/eee.png" alt=""/>
+                <a href="http://andyhume.net/">andyhume.net</a><br/>
+                <a href="http://twitter.com/andyhume/">@andyhume</a>
+            </p>
+
+            <h3>Ben Barnett</h3>
+            <p>
+                <img src="http://dummyimage.com/60x60/ccc/eee.png" alt=""/>
+                <a href="http://benbarnett.net/">benbarnett.net</a><br/>
+                <a href="http://twitter.com/benpbarnett">@benpbarnett</a>
+            </p>
+            
+            <h1>Related Links</h1>
+            <ul>
+                <li>
+                    <h2><a href="http://visitlasvegas.com/">Visit Las Vegas</a></h2>
+                    <p>Your official What Happens in Vegas, Stays in Vegas resource. Find information on hotels, restaurants, golf courses and things to do.</p>
+                </li>
+                <li>
+                    <h2><a href="http://lonelyplanet.com/usa/las-vegas/">Lonely Planet: Las Vegas</a></h2>
+                    <p>Vegas is the ultimate escape. Time is irrelevant here, especially after a few frenzied and intoxicating sleepless nights on the Strip.</p>
+                </li>
+            </ul>
+        </div><!--/complementary-->
+
+    </div><!--/document-->
+
+    <div role="contentinfo" class="contentinfo">
+        <footer>
+            <p class="copyright">
+                <small>
+                    <strong>&#169; <abbr title="2012">2012</abbr> Paul Robert Lloyd</strong><br/>
+                    Code licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/" title="Creative Commons Attribution-Non-Commercial-Share Alike 2.0 UK: England &#38; Wales">Creative Commons BY-NC-SA license</a>.
+                </small>
+            </p>
+        </footer>
+    </div><!--/contentinfo-->
+
+    <!--js-->
+</body>
+</html>
